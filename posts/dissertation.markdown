cb9e5baaa907c5acea514f130199592a8b4648f4
Daniel Mroz <zyxw117@gmail.com>
2019-12-29 01:06:12 +0000

    refactored html

---
+++ posts/dissertation.markdown
@@ -0,0 +1,16 @@
+---
+title: "Categorical semantics of dependent types"
+description: "An introduction for undergraduates"
+date: "2019-07-18"
+lastmod: "2019-07-18"
+---
+## Introduction
+I wrote my masters thesis on the categorical semantics of dependent types. As it stands it is a decent introduction to and motivation for Martin-Lof's theory of dependent types. The bulk of the dissertation consists of a presentation of categories with families and comprehension categories. 
+
+The material is aimed at an advanced undergraduate student who is familiar with the Curry-Howard correspondence between the simply typed lambda calculus and cartesian closed categories.
+
+Unfortunately the work is very brief in some sections, and skips a lot of details. I may expand on it at a later time.
+
+I am happy to receive any questions or corrections.
+
+[Read here](../thesis.pdf)
--- src/Html.hs
+++ src/Html.hs
@@ -5,6 +5,10 @@ import Definitions
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A 
 
+type Template a = a -> H.Html
+
+
+
 postToPage :: Post H.Html -> H.Html
 postToPage  (Post {..}) = html $ do
   H.head $ do
@@ -27,6 +31,28 @@ postToPage  (Post {..}) = html $ do
         H.div ! class_ "footer" $ 
           p $ preEscapedToHtml $ ("Copyright &copy; 2019 Daniel Mroz" :: String)
          
+intro :: H.Html
+intro = do 
+  h1 $ "Daniel Mroz"
+  h4 $ "Computer Science PhD student, Manchester University"
+  h4 $ "Research Interests: Functional programming, logic, category theory"
+  h4 $ do 
+    a ! href "Daniel_Mroz_CV.pdf" $ "CV"
+    " ~ "
+    a ! href "http://github.com/zyxw121" $ "Github"
+  h4 $ "Email: me (at) danielmroz.co.uk"  
+
+about :: H.Html
+about = do
+  h2 $ "About"
+  p $ "After studying Maths and Computer Science at Oxford, I'm now a PhD student at Manchester. I'm a big fan of functional programming, especially in Haskell, because it makes it possible to write very high-level, elegant and understandable programs, and easily reason about the correctness of programs."
+
+posts :: [Post a] -> H.Html
+posts ps = do
+  h2 $ "Posts"
+  ul $ do
+    mapM_ makeListItem  ps 
+
 makeIndex :: [Post a] -> H.Html
 makeIndex ps = html $ do
   H.head $ do
@@ -39,25 +65,16 @@ makeIndex ps = html $ do
       H.div ! class_ "maincontainer container" $ do
         H.div ! class_ "main box" $ do
           H.div ! class_ "center" $ do
-            h1 $ "Daniel Mroz"
-            h4 $ "Maths&CS Undergrad at University College, Oxford"
-            h4 $ "Interests: Functional programming, logic, category theory"
-            h4 $ do 
-              a ! href "Daniel_Mroz_CV.pdf" $ "CV"
-              " ~ "
-              a ! href "http://github.com/zyxw121" $ "Github"
-            h4 $ "Email: daniel.mroz (at) univ.ox.ac.uk"
+            intro
           H.div ! class_ "inside" $ do
-            h2 $ "About"
-            p $ "I am in my fourth year of studying Mathematics and Computer Science at University College, Oxford. I'm a big fan of functional programming, especially in Haskell, because it makes it possible to write very high-level, elegant and understandable programs, and easily reason about the correctness of programs. I'm also interested in logic and category theory."
-            h2 $ "Status"
-            p $ "I am currently working on my dissertation, on the cateorical semantics of Homotopy Type Theory"
-            p $ "Next year I will be studying for a PhD at Manchester"
-            h2 $ "Projects"
-            ul $ do
-              mapM_ makeListItem  ps 
-        H.div ! class_ "footer" $ 
+            about
+            posts ps
+            --h2 $ "Status"
+            --p $ "I am currently working on my dissertation, on the cateorical semantics of Homotopy Type Theory"
+            --p $ "Next year I will be studying for a PhD at Manchester"
+        H.div ! class_ "footer" $ do 
           p $ preEscapedToHtml $ ("Copyright &copy; 2019 Daniel Mroz" :: String)
+          p $ "Last modified: July 18, 2019"
  
 makeListItem :: Post a -> H.Html
 makeListItem Post{..} = html $ li $ do
