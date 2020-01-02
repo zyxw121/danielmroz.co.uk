1b447d3c0df43b91259c63da2d598a6897ca1e11
Daniel Mroz <zyxw117@gmail.com>
2020-01-02 17:56:07 +0000

    added publisher, very rough

--- publish/Main.hs
+++ publish/Main.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 import Html
 import Definitions
@@ -14,6 +16,10 @@ import Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.String
 import System.Directory
 import System.FilePath.Posix
+import System.Environment
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Aeson.Encode.Pretty as P
 
 fromJust :: Maybe a -> a
 fromJust (Just a) = a
@@ -77,16 +83,41 @@ filterMaybes [] = []
 filterMaybes ((Just x):xs) = x : filterMaybes xs
 filterMaybes (Nothing : xs) = filterMaybes xs
 
+getFilePath :: IO (Maybe String)
+getFilePath = getArgs >>= \case 
+  [] -> return Nothing
+  (x:xs) -> return . Just $ x
+
+getTitle :: IO String
+getTitle = do
+  putStr "Title?\n"
+  getLine
+
+lookupTitle :: String -> [Note] -> Maybe Note
+lookupTitle p [] = Nothing
+lookupTitle p (x:xs) = if notePath x == p then Just x else lookupTitle p xs
+
+addNote :: String -> String -> Config a -> Config a
+addNote path title config = let n = Definitions.Note{notePath = path, noteTitle = title} 
+                                in config{notes=n:(notes config)} 
+
+webpath = "website/danielmroz-co-uk"
+
 main :: IO ()
 main = do
-  postpaths <- listDirectory "posts/" 
-  let postnames = map (dropExtension . takeBaseName) postpaths
-  files <- sequence . map (readFile . ("posts/" </>)) $ postpaths
-  let posts = map (runPure . (makePost >=> postToHtml)) $ zip postnames files
-  published <- sequence . map publishPost $ zip postnames posts
-  let published' = filterMaybes published
-      index = makeIndex published' undefined
-  writeFile "public/index.html" (renderHtml index)
-  publishHighlight kate
-
-
+  args <- getArgs
+  let path = head args
+  putStr "Title? \n"
+  s <- getLine
+  home <- getHomeDirectory
+  json <- B.readFile $ home </> webpath </>"config.json"
+  case (eitherDecode json :: Either String (Config String)) of
+    Left e -> putStr ("Error reading config: \n" ++ e) >> return ()
+    Right config -> do
+      case lookupTitle path $ notes config of
+        Just n -> putStr $ "Note already exists with title: " ++ noteTitle n
+        Nothing -> return ()
+      let config' = addNote path s config
+      B.writeFile (home </> webpath </> "config.json") . P.encodePretty$ config'
+      copyFile path $ home </> webpath </> "public/notes" </> path
+      putStr "Done"
