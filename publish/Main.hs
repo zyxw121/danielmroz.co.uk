383ed818653084be6db15867a61e9061a635b7b4
Daniel Mroz <zyxw117@gmail.com>
2020-01-02 20:18:02 +0000

    fixed publisher

--- publish/Main.hs
+++ publish/Main.hs
@@ -29,26 +29,37 @@ lookupTitle p [] = Nothing
 lookupTitle p (x:xs) = if notePath x == p then Just x else lookupTitle p xs
 
 addNote :: String -> String -> Config a -> Config a
-addNote path title config = let n = Definitions.Note{notePath = path, noteTitle = title} 
-                                in config{notes=n:(notes config)} 
+addNote path title config = let n = Definitions.Note{notePath = path, noteTitle = title} in config{notes=n:(notes config)} 
+
+deleteNote' :: String -> [Note] -> [Note]
+deleteNote' p [] = []
+deleteNote' p (x:xs) = if notePath x == p then xs else x:(deleteNote' p xs)
+
+deleteNote :: String -> Config a -> Config a
+deleteNote p config = config{notes = deleteNote' p $ notes config}
 
 webpath = "website/danielmroz-co-uk"
 
 main :: IO ()
 main = do
   args <- getArgs
-  let path = head args
-  putStr "Title? \n"
-  s <- getLine
-  home <- getHomeDirectory
-  json <- B.readFile $ home </> webpath </>"config.json"
-  case (eitherDecode json :: Either String (Config String)) of
-    Left e -> putStr ("Error reading config: \n" ++ e) >> return ()
-    Right config -> do
-      case lookupTitle path $ notes config of
-        Just n -> putStr $ "Note already exists with title: " ++ noteTitle n
-        Nothing -> return ()
-      let config' = addNote path s config
-      B.writeFile (home </> webpath </> "config.json") . P.encodePretty$ config'
-      copyFile path $ home </> webpath </> "public/notes" </> path
-      putStr "Done"
+  case args of
+    [] -> putStr "Usage: publish [file]"
+    (path:xs) -> do
+      let path = head args
+      home <- getHomeDirectory
+      json <- B.readFile $ home </> webpath </>"config.json"
+      case (eitherDecode json :: Either String (Config String)) of
+        Left e -> putStr ("Error reading config: \n" ++ e) >> return ()
+        Right config -> do
+          case lookupTitle path $ notes config of
+            Just n -> do 
+              putStr $ "Note already exists with title: " ++ noteTitle n ++ "\n" 
+              putStr $ "Updating...\n"
+            Nothing -> do
+
+              putStr "Title? \n"
+              s <- getLine
+              B.writeFile (home </> webpath </> "config.json") . P.encodePretty $ addNote path s config
+          copyFile path $ home </> webpath </> "public/notes" </> path
+          putStr "Done"
