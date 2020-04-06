f1ed8becd5e8c139fc475b77ef7a3823153bfd47
Daniel Mroz <zyxw117@gmail.com>
2020-04-06 13:08:04 +0100

    updated info, fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 cd $(dirname $0)
 stack build
 stack exec website
-ssh -i ~/.ssh/dig root@46.101.84.95 service nginx stop
-scp -r public/* suzy:/var/www/html
-ssh -i ~/.ssh/dig root@46.101.84.95 service nginx start
+ssh kitty service nginx stop
+scp -r public/* kitty:/var/www/danielmroz.co.uk/html
+ssh kitty service nginx start
